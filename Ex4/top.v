8ea3dc0a8ec8faad8076019122e69ac0e52049ba
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-04 15:54:03 +0000

    A complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -15,4 +15,29 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+'timescale 1ns / 100ps
 
+module dice(
+	input button,
+	input clk,
+	input rst,
+	output reg [2:0]throw
+	);
+
+	always@(posedge clk)
+	if (rst) begin
+	throw<=3'b000;
+	end
+	else if ((throw==3'b000)||(throw==3'b111))
+	begin
+	throw <=3'b001;
+	end
+	else if(throw==3'b110)
+	begin
+	throw<=(button==1) ? 3'b001 : throw;
+	end
+	else begin
+	throw<=(button==0) ? throw : throw+3'001;
+	end
+
+endmodule
