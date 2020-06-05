a1575e5c547157fe102d9d083fe6f2e825abe6a4
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-05 14:45:44 +0000

    An incomplete solution of Ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -12,5 +12,24 @@
 //  outputs:
 //           result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+module times_table(
+input [2:0]a,
+input [2:0]b,
+input enable,
+input clk,
+wire [5:0]dina
+output [5:0]result
+);
 
+assign result = a*b
+times_table_ex7 your_instance_name (
+  .clka(clk),    // input wire clka
+  .ena(enable),      // input wire ena
+  .wea(0),      // input wire [0 : 0] wea
+  .addra(a),  // input wire [3 : 0] addra
+  .dina(a),    // input wire [15 : 0] dina   
+  .douta(result)  // output wire [15 : 0] douta
+);
+endmodule
 
