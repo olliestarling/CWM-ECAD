88b4c8f22acb603c0227dee3b1757cc8c0c8a2ee
Cloud User <centos@ip-172-31-10-114.eu-west-2.compute.internal>
2020-06-05 11:37:28 +0000

    A complete solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,46 @@
 //  outputs:
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+ module lights(
+ 	input clk,
+ 	output reg red,
+ 	output reg amber,
+ 	output reg green
+ 	);
+
+ 	initial begin
+ 	red=1;
+ 	amber=0;
+ 	green=0;
+
+ 	end
+
+ 	always @(posedge clk)
+ 	if ((red==1)&&(amber==1))
+	begin
+ 	red<=0;
+ 	amber<=0;
+ 	green<=1;
+ 	end
+ 	else if (green==1) 
+	begin
+ 	green<=0;
+ 	amber<=1;
+ 	red<=0;
+ 	end
+ 	else if ((amber==1)&&(red==0))
+	begin
+ 	green<=0;
+ 	red<=1;
+ 	amber<=1;
+ 	end
+ 	else begin
+ 	red<=1;
+ 	amber<=1;
+ 	green<=0;
+
+ 	end
+
+ endmodule
